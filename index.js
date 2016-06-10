c931af93cd986dead19c14c40beb1d193e992ef2
Miguel Madero <git@miguelmadero.com>
2016-06-10 10:48:26 -0300

    Bumps to ember-cli latest

--- index.js
+++ index.js
@@ -2,9 +2,5 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-component-apply',
-
-  included: function () {
-    this.import('bower_components/ember/ember-template-compiler.js');
-  }
+  name: 'ember-component-apply'
 };
